61ae3035287d2430ca672e3d89ea06339731ce76
Douglas Silva <silvatec.br@gmail.com>
2022-01-30 22:04:23 -0300

    update 30-01-2022

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,42 +1,98 @@
-var diryJ,dirxJ;
+//VARIÁVEIS GLOBAIS
+var jog,dirxJ,diryJ,velJ,pjx,pjy;
+var tamanhoTelaW,tamanhoTelaH;
+var jogo;
+var frames;
 
+//FUNÇÃO QUANDO A TECLA ESTÁ PRESSIONADA 
 function teclaDw(){
-    var tecla = event.keyCode;
 
+	var tecla=event.keyCode;
+    
     //TECLA PARA CIMA
-    if(tecla==38){
-        diryJ=-1;
+	if(tecla==38){
+		diryJ=-1;
 
-    //TECLA PARA BAIXO    
-    }else if(tecla==40){
-        diryJ=1;
-    }
+    //TECLA PARA BAIXO
+	}else if(tecla==40){
+		diryJ=1;
+	}
+
+    //TECLA PARA DIREITA
+	if(tecla==39){
+		dirxJ=1;
 
     //TECLA PARA ESQUERDA
-    if(tecla==37){
-        dirxJ=-1;
+	}else if(tecla==37){
+		dirxJ=-1;
+	}
 
-    //TECLA PARA DIREITA    
-    }else if(tecla==39){
-        dirxJ=1;
-    }
-
-    //TECLA DE ESPAÇO - TIRO
-    if(tecla==32){
-
-    }
+    //TECLA ESPAÇO - PARA ATIRAR
+	if(tecla==32){
+		
+	}
 }
 
+//FUNÇÃO QUANDO A TECLA NÃO ESTÁ PRESSIONADA
 function teclaUp(){
-    var tecla = event.keyCode;
 
-    if((tecla==38)||(tecla==40)){
-        diryJ=0;
+	var tecla=event.keyCode;
 
-    }
+    //TECLA CIMA, BAIXO
+	if((tecla==38)||(tecla==40)){
+		diryJ=0;
+	}
 
-    if((tecla==37)||(tecla==39)){
-        dirxJ=0;
-
-    }
+    //TECLA DIREITA, ESQUERDA
+	if((tecla==39)||(tecla==37)){
+		dirxJ=0;
+	}
 }
+
+//FUNÇÃO DE CONTROLE DO JOGADOR
+function controlaJogador(){
+    pjy+=diryJ*velJ;
+	pjx+=dirxJ*velJ;
+	jog.style.top=pjy+"px";
+	jog.style.left=pjx+"px";
+}
+
+//LOOP PRINCIPAL DO GAME
+function gameLoop(){
+
+    if(jogo){
+        //FUNÇÕES DE CONTROLE
+        controlaJogador();
+    }
+
+    //CONTROLE DE FRAMES DO GAME
+    frames=requestAnimationFrame(gameLoop);
+
+}
+
+//INICIO DE TODOS OS COMPONENTES DO GAME
+function inicia(){
+
+    //INICIO DO GAME
+    jogo=true;
+
+    //INICIAR TELA DE JOGO
+    tamanhoTelaW=window.innerWidth;
+	tamanhoTelaH=window.innerHeight;
+
+    //INICIAR JOGADOR
+    dirxJ=diryJ=0;
+	pjx=tamanhoTelaW/2;
+	pjy=tamanhoTelaH/2;
+	velJ=5;
+	jog=document.getElementById("naveJog");
+	jog.style.top=pjy+"px";
+	jog.style.left=pjx+"px";
+
+    gameLoop();
+}
+
+//EVENTOS
+window.addEventListener("load",inicia);
+document.addEventListener("keydown",teclaDw);
+document.addEventListener("keyup",teclaUp);
